4666d46f54d4ea029fdcc05ed838d489370330c1
shreya-lang <shs058347@gmail.com>
2026-05-26 23:53:56 +0530

    fix render port and lazy load ocr

--- backend/app/ocr_service.py
+++ backend/app/ocr_service.py
@@ -1,13 +1,15 @@
 import easyocr
 
-reader = easyocr.Reader(['en'])
+reader = None
 
 def extract_text(image_path):
+    global reader
+
+    if reader is None:
+        reader = easyocr.Reader(["en"])
 
     result = reader.readtext(image_path)
 
-    text = " ".join(
-        [item[1] for item in result]
-    )
+    text = " ".join([item[1] for item in result])
 
     return text
